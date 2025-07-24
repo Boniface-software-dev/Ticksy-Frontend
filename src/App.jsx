2a784418c72cbe78f2f84c4b27917dc2462d775d
Edwin kipyego <koriredwin95@gmail.com>
2025-07-25 01:29:11 +0300

    add events page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../src/pages/Login";
 import Register from "../src/pages/Register";
 import OrgProfile from "./pages/organizer/OrgProfile";
+import Events from "./pages/events";
+import EventsList from "./pages/EventsList";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { loadUser } from "./features/authentification/authSlice";
@@ -20,6 +22,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin/:id/profile" element={<OrgProfile />} />
+        <Route path="/events/:id" element={<Events />} />
+        <Route path="/events" element={<EventsList />} />
+        
       </Routes>
     </Router>
   );
