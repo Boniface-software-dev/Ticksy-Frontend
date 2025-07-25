9b551c7d338424dfab70257c34549c41fc46da1f
Edwin kipyego <koriredwin95@gmail.com>
2025-07-25 20:35:21 +0300

    remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import AdminSidebar from "./components/AdminSidebar";
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
+import OrgProfile from "./pages/organizer/OrgProfile";
+
+import Events from "./pages/events";
+import EventsList from "./pages/EventsList";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { loadUser } from "./features/authentification/authSlice";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex min-h-screen">
-        <AdminSidebar />
-        <div className="flex-1 flex items-center justify-center text-2xl text-gray-400">
-          This is the main content area
-        </div>
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin/:id/profile" element={<OrgProfile />} />
+        
+        <Route path="/events/:id" element={<Events />} />
+        <Route path="/events" element={<EventsList />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
