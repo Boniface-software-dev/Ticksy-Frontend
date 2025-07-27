b7284fdc6fba4c7fa9af14e4699541ae779db477
jedaqsaul <jedaqsaul@gmail.com>
2025-07-27 11:41:31 +0300

    add admin profile

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,10 @@ function App() {
         <Route path="/admin/:id/profile" element={<OrgProfile />} />
         <Route path="/events/:id" element={<Events />} />
         <Route path="/events" element={<EventsList />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/:id/dashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
