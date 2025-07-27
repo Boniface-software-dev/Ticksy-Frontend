e1b29f513b2a1a2ffa24b91b82e2ffa2ac9f8517
Joy Malinda <kokimalinda@gmail.com>
2025-07-27 17:46:58 +0300

    main.jsx change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client"
 import './index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux"
-//import { BrowserRouter } from 'react-router-dom'
 import store from "./app/store.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
