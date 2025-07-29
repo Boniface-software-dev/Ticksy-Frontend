58dfffd305449b6caf9d40b82e386d95d659d5d7
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-07-29 13:20:03 +0300

    Updated store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../features/authentification/authSlice'
 import eventReducer from '../features/events/eventSlice';
+import ticketsReducer from '../features/tickets/ticketsSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     events: eventReducer,
+    tickets: ticketsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
